397ffa2afd42e3ea7fcf15dab391528c7f5b9589
Jacob Lara <l1a9r9a7@gmail.com>
2019-11-13 11:24:36 -0700

    Added js data object and started server js file

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+const tableData = [
+    {
+    customerName: "Jacob",
+    phoneNumber: "000-000-0000",
+    customerEmail: "dhsfl@kja.com",
+    customerID: "2020"
+    }
+];
+
+module.exports = tableData;
